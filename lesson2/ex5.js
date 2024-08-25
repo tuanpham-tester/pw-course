5d98f3bc80e3c934fd8964954ce1ab43f1391b5d
tuanpham-tester <tuanpham91.tester@gmail.com>
2024-08-25 22:54:15 +0700

    init project

--- lesson2/ex5.js
+++ lesson2/ex5.js
@@ -9,6 +9,7 @@ for (let i = 1; i <= 100; i++) {
         console.log("Số", i , "là số chẵn");
     }
 }
+
 for (let i = 1; i <= 100; i++) {
     if (i % 2 !== 0) {
         console.log("Số", i , "là số lẻ");
