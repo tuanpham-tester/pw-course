3ce8048170810cf727c45c6a833f3c0faf5672ff
tuanpham-tester <tuanpham91.tester@gmail.com>
2024-09-16 22:35:36 +0700

    Bai tap lesson-3

--- lesson-3/01-object.js
+++ lesson-3/01-object.js
@@ -10,7 +10,7 @@ console.log("năm sản xuất của xe:", car["year"]);
 //Bài 2:
 let person = {
     name: "Joon Pham",
-    address:  {
+    address: {
         street: "123 Yen Xa",
         city: "Ha Noi",
         country: "Viet Nam"
@@ -22,12 +22,12 @@ console.log("Tên đường:", person["address"]["street"]);
 //Bài 3:
 let student = {
     name: "Joon Pham",
-    grades:  {
+    grades: {
         math: 8,
         english: 9
-        }
     }
-    console.log("Điểm toán:", student["grades"]["math"]);
+}
+console.log("Điểm toán:", student["grades"]["math"]);
 
 //Bài 4:
 let product = {
@@ -36,5 +36,39 @@ let product = {
     "motobike Yamaha": 1500
 }
 for (let productsName in product) {
-    console.log(`Tên sản phẩm: ${productsName}, Giá: ${product[productsName]} USD`); 
+    console.log(`Tên sản phẩm: ${productsName}, Giá: ${product[productsName]} USD`);
 }
+
+// Bài 5:
+let settings = {
+    volume: 50,
+    brightness: 70
+};
+console.log("Cài đặt ban đầu:", settings);
+settings.volume = 80;
+console.log("Cài đặt sau khi thay đổi volume:", settings);
+
+//Bài 6:
+let bike = {
+    brand: "Yamaha",
+    model: "MT-07"
+};
+console.log("Thông tin xe ban đầu :", bike);
+bike.color = "Đen";
+console.log("Thông tin xe sau khi thêm màu sắc:", bike);
+
+//BÀi 7:
+let employee = {
+    name: "Phạm Văn Tuấn",
+    age: 33
+};
+console.log("Thông tin nhân viên ban đầu:", employee);
+delete employee.age;
+console.log("Thông tin nhân viên sau khi xóa tuổi:", employee);
+
+//Bài 8:
+let school = {
+    classA: ["An", "Bình", "Châu"],
+    classB: ["Đào", "Hương", "Giang"]
+};
+console.log("Thông tin trường học:", school);
