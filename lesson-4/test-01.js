61f1d603b16dcb3c932282bb59e45f32fe2bd1f1
tuanpham-tester <tuanpham91.tester@gmail.com>
2024-09-20 23:45:39 +0700

    Update bài kiểm tra test-01

--- lesson-4/test-01.js
+++ lesson-4/test-01.js
@@ -1,5 +1,7 @@
 // Bài 1
 
+const { parse } = require("path");
+
 let departurePlanet = "Trái Đất";
 let misson = "Khám phá Vũ trụ K9";
 let crew = ["Joon Phạm 1", "Joon Phạm 2", "Joon Phạm 3"];
@@ -12,29 +14,27 @@ console.log(launchShip(crew));
 // Bài 2
 
 function calculateDistance(speed, time) {
-    let distance = speed * time;
-    return (distance);
+    if ((typeof speed === 'number') && (typeof time === 'number')) {
+        return (speed * time);
+    } else {
+        return (`Tham số không hợp lệ, vui lòng kiểm tra lại!`);
+    }
 }
-
-let calculateDistance1 = calculateDistance(1000, 24);
-console.log(`Khoảng cách tàu vũ trụ K9 đến hành tinh bí ẩn là: ${calculateDistance1}km`);
-
-//cách 2:
-
-function calculateDistance(speed, time) {
-    return (speed * time);
-}
-
 let speed = 1000; // km/h
 let time = 24; // giờ
 let distance = calculateDistance(speed, time);
 console.log(`Khoảng cách tàu vũ trụ K9 đến hành tinh bí ẩn là: ${distance} km`);
 
+
 // Bài 3:
 
 function convertTimeToHex(decimalNumber) {
-    let hexadecimalNumber = decimalNumber.toString(16);
-    return (hexadecimalNumber);
+    if (!isNaN (decimalNumber)) {
+        let hexadecimalNumber = parseInt.toString(16);
+        return (hexadecimalNumber);
+    } else {
+        return (`Tham số không hợp lệ. Mời nhập lại`);
+    }
 }
 let convertTimeToHex1 = convertTimeToHex(120);
 console.log(`Thời gian 120 giây trong hệ thập lục phân là: ${convertTimeToHex1}`);
@@ -42,7 +42,11 @@ console.log(`Thời gian 120 giây trong hệ thập lục phân là: ${convertT
 //hoặc
 
 function convertTimeToHex(decimalNumber) {
-    return decimalNumber.toString(16);
+    if (typeof decimalNumber === 'number') {
+        return decimalNumber.toString(16);
+    } else {
+        return (`Tham số không hợp lệ. Mời nhập lại`);
+    }
 }
 let decimalNumber = 120;
 let hexadecimalNumber = convertTimeToHex(decimalNumber);
@@ -65,26 +69,10 @@ let code = "K9 Challenge";
 let decryptedCode = decryptCode(code);
 console.log(`Mật mã đã được giải mã là: ${decryptedCode}`);
 
-// cách 2: khai báo 1 mảng => so sánh từng thành phần trong mảng
-// function decryptCode(code) {
-//     let arr = code.split('');
-//     let arr1 = '';
-//     for (i = 0; i < arr.lenght; i++) {
-//         if (arr[i] === text.toLowerCase()) {
-//             arr1 += text.toUpperCase();
-//         } else {
-//             arr1 += text.toLowerCase();
-//         }
-//     }
-//     return arr1;
-// }
-// let code = 'K9 Challenge';
-// let decryptedCode = decryptCode(code);
-// console.log(`Mật mã đã được giải mã là: ${decryptedCode}`);
 
 //Bài 5:
 
 function returnToEarth() {
     console.log("Chuẩn bị trở về Trái Đất!");
-    }
-    returnToEarth();
+}
+returnToEarth();
