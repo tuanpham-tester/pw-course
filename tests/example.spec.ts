6f27c3088dfd6daa1777803ed268011012432fb3
k9-phamvantuan <tuanpham91.tester@gmail.com>
2024-09-24 13:08:18 +0700

    Update bài tập lesson-5

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,18 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://material.playwrightvn.com/');
+  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Tài liệu học automation test/);
+  await expect(page).toHaveTitle(/Playwright/);
 });
 
 test('get started link', async ({ page }) => {
-  await page.goto('https://material.playwrightvn.com/ ');
+  await page.goto('https://playwright.dev/');
 
   // Click the get started link.
-  await page.getByRole('link', { name: 'Bài học 1: Register Page' }).click();
+  await page.getByRole('link', { name: 'Get started' }).click();
 
   // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'User Registration' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
--- tests/lesson-5/test1.spec.ts
+++ tests/lesson-5/test1.spec.ts
@@ -1,12 +1,9 @@
 import { test, expect } from '@playwright/test';
-
 test('bài số 1', async ({ page }) => {
     await page.goto('https://material.playwrightvn.com/');
     await page.locator('//a[contains(text(),"Bài học 1: Register Page (có đủ các element)")]').click();
     await page.locator('//input[@id="username"]').fill('joonpham');
-    await page.locator('//input[@id="email"]').pressSequentially('tuanpham@gmail.com', {
-        delay: 100
-    });
+    await page.locator('//input[@id="email"]').fill('tuanpham@gmail.com');
     await page.locator('//input[@id="male"]').click();
     await page.locator('//input[@id="reading"]').check();
     await page.locator('//input[@id="traveling"]').check();
@@ -17,7 +14,7 @@ test('bài số 1', async ({ page }) => {
     await page.locator('//textarea[@id="bio"]').fill('Kiểm tra bài test playwright số 1');
     await page.locator('//input[@id="rating"]').fill("3");
     await page.locator('//input[@id="favcolor"]').fill("#fff000");
-    await page.locator('//input[@id="newsletter"]').check();
+    await page.locator('//input[@id="newsletter"]').click();
     await page.locator('//span[@class="slider round"]').click();
     await page.locator('//button[@type="submit"]').click();
 });
